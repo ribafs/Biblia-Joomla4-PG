04168accc19f88e714fc637ad1a646c9fead81f7
Ribamar FS <ribafs@gmail.com>
2022-06-12 12:59:30 -0300

    update

---
+++ com_bibliant/install.bibliant.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @ install.bibliant.php
+ * @author-name Ribamar FS
+ * @copyright	Copyright (C) 2012 Ribamar FS.
+ * @license GNU/GPL, see http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ */
+
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+function com_install()
+{
+	?>
+	<div class="header">Parabéns, o Componente Versículo do dia (NT) está instalado!</div>
+	<p>
+		Agora a cada chagada ao site o visitante será recebido por um versículo diferente do novo testamento.
+	</p>
+	<?php
+}
+
+?>
---
+++ com_bibliant/uninstall.bibliant.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @ uninstall.bibliant.php
+ * @ author-name Ribamar FS
+ * @ copyright	Copyright (C) 2012 Ribamar FS.
+ * @ license GNU/GPL, see http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ */
+
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+function com_uninstall()
+{
+	?>
+	<div class="header">O Versículo do dia (NT) agora foi removido do seu sistema.</div>
+	<p>
+	O Versículo do dia agora foi removido do seu sistema.
+	</p>
+	<?php
+}
+
+?>
